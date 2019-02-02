c9bdb93f02fee29574af099598985a0e56348173
NajamShehzad <najambutt195@gmail.com>
2019-02-02 23:19:05 +0500

    Socket.io Client + Chat

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import SocketIOClient from 'socket.io-client';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,7 +24,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get started, edit App.js Chat</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
---
+++ components/Chat/Chat.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <View>
+                <Text>
+                    Najam
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default Chat;
