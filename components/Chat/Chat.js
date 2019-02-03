4336ce7a1671410bdafbc8611c8d02d8beaba83f
NajamShehzad <najambutt195@gmail.com>
2019-02-03 12:00:45 +0500

    EsLint Init Completed

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,18 +1,17 @@
-
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
 class Chat extends Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+
     render() {
         return (
             <View>
                 <Text>
-                    Najam
-                </Text>
+                    Name
+              </Text>
             </View>
         );
     }
